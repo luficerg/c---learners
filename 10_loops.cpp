f2e8dab39c1d0aec27d207a2fe5d06aaad0e8991
Luficer G <luficerg5@gmail.com>
2024-05-13 13:34:52 +0530

    commit

---
+++ 10_loops.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // // FOr loop
+    // for (int i = 0; i < 100; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+    
+    // // While loop
+    // int j = 0;
+    // while(j<100){
+    //     cout<<j<<endl;
+    //     j++;
+
+    // }
+
+    // // DO while
+
+    // int k = 0;
+    // do{
+    //     cout<<"This will run only once.."<<endl;
+    //     cout<<k<<endl;
+    //     k++;
+    // }while(false);
+
+
+    // Exericse
+    for (int i = 1; i < 11; i++)
+    {
+        cout<<"6 X "<<i<<" is "<<6*i<<endl;
+    }
+    
+
+    return 0;
+}
